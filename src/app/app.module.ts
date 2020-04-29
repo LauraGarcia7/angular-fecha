305948b60ae908847594f69f14c1bf69b7b1a690
LauraGarcia7 <laurav.garcia@correo.usa.edu.co>
2020-04-29 12:55:39 -0500

    Componentes fecha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
-import { LoginComponent } from "./login/login.component";
-import { RegistroComponent } from "./registro/registro.component";
-import { Pregunta1Component } from './pregunta1/pregunta1.component';
-import { Pregunta2Component } from './pregunta2/pregunta2.component';
-import { Pregunta3Component } from './pregunta3/pregunta3.component';
-import { Pregunta4Component } from './pregunta4/pregunta4.component';
 import { FechaComponent } from './fecha/fecha.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, LoginComponent,RegistroComponent, Pregunta1Component, Pregunta2Component, Pregunta3Component, Pregunta4Component, FechaComponent],
+  declarations: [AppComponent, HelloComponent, FechaComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
